dbe5a23856fa1ba0e5b87909193cf106d8e4d783
JorgeMioska <quispechilojorge@gmail.com>
2023-02-02 21:33:40 -0500

    task removing

--- src/conponents/TaskCard.jsx
+++ src/conponents/TaskCard.jsx
@@ -1,10 +1,14 @@
 
 
 export default function TasCard({task}) {
+  function deleteTask(){
+    alert("task to eliminate "+ "{ "+task.id+" }")
+  }
   return (
     <div>
       <h2>{task.task}</h2>
       <p>{task.description}</p>
+      <button onClick={deleteTask}>Complete and Delete</button>
     </div>
   );
 }

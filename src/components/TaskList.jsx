9d55748290ad656f333f1839beabf5d05ec2bb2a
JorgeMioska <quispechilojorge@gmail.com>
2023-02-06 03:50:01 -0500

    react useContenxt Tasklist

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,16 +1,19 @@
 import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
-const TaskList = ({ tasks, deleteTask }) => {
+function TaskList() {
+  const {tasks} = useContext(TaskContext);
   if (tasks.length === 0) {
     return <div>No tasks yet</div>;
   }
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask} />
+        <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
-};
+}
 
 export default TaskList;
